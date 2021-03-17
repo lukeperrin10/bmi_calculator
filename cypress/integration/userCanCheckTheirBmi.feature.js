e466573dbafc2eb51398a2853d71d60b2ba6b555
Luke <lukeperrin27@gmail.com>
2021-03-17 21:26:09 +0100

    added js files to assets for UI and BMI calculator, ran second test in cypress.

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -5,6 +5,7 @@
 describe('User can check their BMI', () => {
     it('user can enter weight and height', () => {
             cy.visit('http://localhost:3001')
+            cy.contains('BMI Calculator')
 
     })
 
