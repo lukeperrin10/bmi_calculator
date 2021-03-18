a811f722ad4ab7f6d60bd8cf9d8649bcf77e0bc9
Luke <lukeperrin27@gmail.com>
2021-03-18 14:24:20 +0100

    multiple chages added, including more BMI value groups, some small CSS styling. Trying to work on some edge cases when value enter is not correct

--- src/assets/js/bmiCalculator.js
+++ src/assets/js/bmiCalculator.js
@@ -1,21 +1,31 @@
 
-
-
 const calculateMetricBMI = (weight, height) => {
     let result
     result = weight / ((height/100)*(height / 100))
-    return result
+    return (Math.round(result*100) / 100).toFixed(2)
 }
 
 const getBMIclassification = (value) => {
-    if (value < 18.5){
+
+    if (value > 1 && value < 18.5){
         return "underweight"
-    }else if (value < 25) {
-        return "normal"
     }
-    else if (value > 25) {
+    else if (value >= 18.5 && value <= 24.9) {
+        return "within normal paremeters"
+    }
+    else if (value >= 25 && value <= 29.9) {
         return "overweight"
     }
+    else if (value >= 30 && value <= 34.9) {
+        return "Obesity Class 1"
+    }
+    else if (value >= 35 && value <= 39.9) {
+        return "Obesity Class 2"
+    }
+    else if (value > 40) {
+        return "Extreme Obesity Class 3"
+    }
+
 }
 
 
--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -2,13 +2,14 @@
 
 const calculateBMI = () => {
 
-
+    
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let results = calculateMetricBMI(weightInput.value, heightInput.value)
     let message = getBMIclassification(results)
     let displayResultsElement = document.getElementById('results')
-    displayResultsElement.innerHTML = `<h1> Your BMI value is: ${results} and you are within ${message}</h1>`
+    displayResultsElement.innerHTML = `<h1 id="resultdisplay"> Your BMI value is: ${results} and you are ${message}.</h1>`
 
 }
+
 
